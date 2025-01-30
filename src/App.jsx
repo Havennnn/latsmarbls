e96f7c1bb2ecb9f7f80ba3ca0cffa367df3af722
Havennnn <latrellmarable00@gmail.com>
2025-01-30 17:11:43 +0800

    preloade and app adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 5000); // Adjust as needed for loading duration
+    }, 3000); // Adjust as needed for loading duration
   }, []);
 
   return (
--- src/component/Preloader.jsx
+++ src/component/Preloader.jsx
@@ -26,7 +26,7 @@ const Preloader = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0, scale: 0.9 }}
-        transition={{ duration: 1.5, ease: "easeInOut" }}
+        transition={{ duration: 1, ease: "easeInOut" }}
       />
     </div>
   );
